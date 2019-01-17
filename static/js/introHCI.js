71f4cbab32ad93013c97f8bebfb947ddc3522180
Yimeng Sun <yms001@ucsd.edu>
2019-01-17 10:25:16 -0800

    added Javascript

---
+++ static/js/introHCI.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript is connected");
+	});
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
